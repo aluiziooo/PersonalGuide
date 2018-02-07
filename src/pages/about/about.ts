765b52263624ad12b6f48960a647bea98fbcff27
Aluizio <aluizioneto03@gmail.com>
2018-02-07 11:16:56 -0300

    Primeiro COmmit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListasPage } from '../listas/listas';
 
 @Component({
   selector: 'page-about',
@@ -9,6 +10,9 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  abrirListas(){
+    this.navCtrl.push(ListasPage);
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListasPage } from '../listas/listas';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
 
+  }
+  abrirListas(){
+    this.navCtrl.push(ListasPage);
   }
 
 }
